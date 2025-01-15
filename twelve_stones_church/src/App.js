c737417e4f54cf2bd15049d9fdeedcd9680de35b
parkjiu0208 <joy002208@gmail.com>
2025-01-15 23:51:47 +0900

    nav 추가

--- twelve_stones_church/src/App.js
+++ twelve_stones_church/src/App.js
@@ -1,11 +1,21 @@
 import Nav from './component/common/Nav';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import WordPage from './pages/WordPage';
+import HymnPage from './pages/HymnPage';
+import GallaryPage from './pages/GallaryPage';
 
 function App() {
   return (
     <div className="app-container">
       <Nav />
       <div className="content-container">
-        hello
+      <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/word" element={<WordPage />} />
+          <Route path="/hymn" element={<HymnPage />} />
+          <Route path="/gallary" element={<GallaryPage />} />
+        </Routes>
       </div>
     </div>
   );
--- twelve_stones_church/src/component/common/Nav.jsx
+++ twelve_stones_church/src/component/common/Nav.jsx
@@ -5,10 +5,10 @@ import logo from '../../img/열두돌교회로고.png';
 const Nav = () => {
   return (
     <div className="flex justify-center">
-      <div className="container flex justify-between items-center">
+      <div className="container flex justify-between w-1 h-1 items-center">
         <Link to="/">
           <div className="flex items-center">
-            <img src={logo} className="h-10" alt="열두돌교회로고" />
+            <img src={logo} className="h-0.1" alt="열두돌교회로고" />
           </div>
         </Link>
         <ul className="flex space-x-2 md:space-x-3 ml-9 text-sm">
@@ -21,7 +21,43 @@ const Nav = () => {
                 }`
               }
             >
-              Home
+              교회소개
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/word"
+              className={({ isActive }) =>
+                `flex flex-col items-center ${
+                  isActive ? "text-sky-600 border-b border-sky-600" : "text-black"
+                }`
+              }
+            >
+              말씀
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/Hymn"
+              className={({ isActive }) =>
+                `flex flex-col items-center ${
+                  isActive ? "text-sky-600 border-b border-sky-600" : "text-black"
+                }`
+              }
+            >
+              찬양
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/gallary"
+              className={({ isActive }) =>
+                `flex flex-col items-center ${
+                  isActive ? "text-sky-600 border-b border-sky-600" : "text-black"
+                }`
+              }
+            >
+                사진첩
             </NavLink>
           </li>
         </ul>
