ca632352bea4a0211ce22293dcc5959d5229b932
parkjiu0208 <joy002208@gmail.com>
2025-01-18 23:58:13 +0900

    메인에 표어 추가

--- twelve_stones_church/src/pages/HomePage.jsx
+++ twelve_stones_church/src/pages/HomePage.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 
 const HomePage = () => {
-
-    return (
-        <section id="home" className="py-16 bg-white">
-          <div className="container mx-auto px-4">
-            <h2 className="text-3xl font-bold text-gray-800 mb-8 text-center">메인페이지</h2>
-            <div className="max-w-3xl mx-auto">
-              <p className="text-gray-600 mb-4 text-center">
-                메인페이지입니다.
-              </p>
+  return (
+    <section id="home" className="py-16 bg-white">
+      <div className="container mx-auto px-4">
+        <h2 className="text-3xl font-bold text-gray-800 mb-8 text-center">메인페이지</h2>
+        <div className="max-w-3xl mx-auto">
+          <div className="flex justify-center items-center">
+            <div className="rounded-full bg-gray-200 text-gray-800 text-center py-4 px-8">
+              2025 표어
             </div>
           </div>
-        </section>
-      )
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default HomePage;
